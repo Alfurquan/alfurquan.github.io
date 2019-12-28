ee040a4c855b9a1028205435e5fc77f27b363af5
Alfurquan <zahedialfurquan20@gmail.com>
2019-12-28 21:59:11 +0530

    Changes

--- js/index.js
+++ js/index.js
@@ -1,95 +1,49 @@
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $("header").outerHeight();
 
-$(document).ready(function() {
-  $(window).scroll(function(event) {
-    didScroll = true;
-  });
+$(document).ready(function(){
 
-  $("nav")
-    .find("a")
-    .click(function(e) {
-      e.preventDefault();
-      $("nav")
-        .find(".active")
-        .removeClass("active");
-      $(this).addClass("active");
-      var section = $(this).attr("href");
-      $("html, body").animate({
-        scrollTop: $(section).offset().top
-      });
-    });
+    $('#js-navbar-toggle').on('click',function (){
+       $('.menu').toggleClass('open')
+    })
 
-  setInterval(function() {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }, 250);
 
-  function hasScrolled() {
-    var st = $(this).scrollTop();
 
-    // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta) return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
-      // Scroll Down
-      $("header")
-        .removeClass("nav-down")
-        .addClass("nav-up");
+  //code to make sticky navbar
+  var sticky = $('.topnav').offset().top;
+  $(window).bind('scroll', function () {
+    if ($(window).scrollTop() >= sticky) {
+        $('.topnav').addClass('sticky');
     } else {
-      // Scroll Up
-      if (st + $(window).height() < $(document).height()) {
-        $("header")
-          .removeClass("nav-up")
-          .addClass("nav-down");
-      }
+        $('.topnav').removeClass('sticky');
     }
 
-    lastScrollTop = st;
-  }
-
-  $(window).scroll(function() {
-    /* Check the location of each desired element */
-    $(".hideme").each(function(i) {
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      /* If the object is completely visible in the window, fade it it */
-      if (bottom_of_window > bottom_of_object) {
-        $(this).animate({ opacity: "1" }, 500);
+    //SCROLL ANIMATIONS
+    function onScrollInit( items, elemTrigger ) {
+        var offset = $(window).height() / 1.6
+        items.each( function() {
+          var elem = $(this),
+              animationClass = elem.attr('data-animation'),
+              animationDelay = elem.attr('data-delay');
+    
+              elem.css({
+                '-webkit-animation-delay':  animationDelay,
+                '-moz-animation-delay':     animationDelay,
+                'animation-delay':          animationDelay
+              });
+    
+              var trigger = (elemTrigger) ? trigger : elem;
+    
+              trigger.waypoint(function() {
+                elem.addClass('animated').addClass(animationClass);
+                if (elem.get(0).id === 'gallery') mixClear(); //OPTIONAL
+                },{
+                    triggerOnce: true,
+                    offset: offset
+              });
+        });
       }
-    });
-  });
+    
+      setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
+
 });
 
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "nav-down") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "nav-down";
-//   }
-// }
-
-function openContent(evt, contentName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(contentName).style.display = "block";
-  $(contentName).fadeIn("slow");
-  evt.currentTarget.className += " active";
-}
-
-document.getElementById("defaultOpen").click();
+})
