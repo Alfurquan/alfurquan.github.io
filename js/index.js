b234a1c5a031043817df96c7beadfa70bf1cc870
Alfurquan <zahedialfurquan20@gmail.com>
2019-12-29 18:38:32 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -10,6 +10,22 @@ $(document).ready(function(){
   //code to make sticky navbar
   var sticky = $('.topnav').offset().top;
   $(window).bind('scroll', function () {
+
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    var scrollBottom = pos + $(window).height();
+
+    // Link Highlighting
+    if (pos2 > $('#home').offset().top)       { highlightLink('home'); }
+    if (pos2 > $('#about').offset().top)      { highlightLink('about'); }
+    if (pos2 > $('#experience').offset().top)  { highlightLink('experience'); }
+    if (pos2 > $('#work').offset().top)       { highlightLink('work'); }
+    if (pos2 > $('#contact').offset().top ||
+        pos + $(window).height() === $(document).height()) {
+          highlightLink('contact');
+    }
+
+
     if ($(window).scrollTop() >= sticky) {
         $('.topnav').addClass('sticky');
     } else {
@@ -45,5 +61,25 @@ $(document).ready(function(){
       setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
 
 });
+
+function highlightLink(anchor) {
+  $('.menu .active').removeClass('active');
+  $(".menu").find('[dest="' + anchor + '"]').addClass('active');
+}
+
+// EVENT HANDLERS
+$('.nav-links').click(function() {
+  var anchor = $(this).attr("dest");
+   $('.menu').removeClass('open');
+
+  $('.menu .active').removeClass('active');
+  $(".menu").find('[dest="' + anchor + '"]').addClass('active');
+
+  $('html, body').animate({
+    scrollTop: $('#' + anchor).offset().top
+  }, 400);
+});
+
+
 
 })
